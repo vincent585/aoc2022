ede71142af18adafcf5ff2c6af11bfb8cdf9cf0a
vklimjack@gmail.com <vklimjack@gmail.com>
2022-12-04 08:49:01 +0000

    day 4 part1 solution

---
+++ AdventOfCodeDay4/Program.cs
@@ -0,0 +1,34 @@
+﻿var sectionPairs = File.ReadAllLines("C:\\Users\\vklim\\OneDrive\\Desktop\\Advent Of Code 2022\\aocday4.txt");
+
+var redundantPairs = 0;
+
+foreach (var pair in sectionPairs)
+{
+    var pairArray = pair.Split(',');
+
+    var firstElf = pairArray[0].Split('-');
+    var secondElf = pairArray[1].Split('-');
+
+    var firstElfRange = Enumerable.Range(int.Parse(firstElf[0]), int.Parse(firstElf[1]) - int.Parse(firstElf[0]) + 1);
+    var secondElfRange = Enumerable.Range(int.Parse(secondElf[0]), int.Parse(secondElf[1]) - int.Parse(secondElf[0]) + 1);
+
+    IEnumerable<int>? smallerRange = null;
+    IEnumerable<int>? widerRange = null;
+
+    if (firstElfRange.Count() >= secondElfRange.Count())
+    {
+        widerRange = firstElfRange;
+        smallerRange = secondElfRange;
+    }
+    else
+    {
+        widerRange = secondElfRange;
+        smallerRange = firstElfRange;
+    }
+
+    if (smallerRange.All(x => widerRange.Contains(x))) redundantPairs++;
+
+
+}
+
+return redundantPairs;
