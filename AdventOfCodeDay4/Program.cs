8645b01ccbd5978533a1966c6942ccdfa68cdb4f
vklimjack@gmail.com <vklimjack@gmail.com>
2022-12-04 08:54:22 +0000

    day 4 part 2 solution

--- AdventOfCodeDay4/Program.cs
+++ AdventOfCodeDay4/Program.cs
@@ -12,6 +12,14 @@ foreach (var pair in sectionPairs)
     var firstElfRange = Enumerable.Range(int.Parse(firstElf[0]), int.Parse(firstElf[1]) - int.Parse(firstElf[0]) + 1);
     var secondElfRange = Enumerable.Range(int.Parse(secondElf[0]), int.Parse(secondElf[1]) - int.Parse(secondElf[0]) + 1);
 
+    // redundantPairs += PartOneSolution(firstElfRange, secondElfRange);
+    redundantPairs += PartTwoSolution(firstElfRange, secondElfRange);
+}
+
+return redundantPairs;
+
+int PartOneSolution(IEnumerable<int> firstElfRange, IEnumerable<int> secondElfRange)
+{
     IEnumerable<int>? smallerRange = null;
     IEnumerable<int>? widerRange = null;
 
@@ -26,9 +34,14 @@ foreach (var pair in sectionPairs)
         smallerRange = firstElfRange;
     }
 
-    if (smallerRange.All(x => widerRange.Contains(x))) redundantPairs++;
+    if (smallerRange.All(x => widerRange.Contains(x))) return 1;
 
-
+    return 0;
 }
 
-return redundantPairs;
+int PartTwoSolution(IEnumerable<int> firstElfRange, IEnumerable<int> secondElfRange)
+{
+    if (firstElfRange.Any(x => secondElfRange.Contains(x))) return 1;
+
+    return 0;
+}
