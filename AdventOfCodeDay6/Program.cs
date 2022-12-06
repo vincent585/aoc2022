45633e44352de5fb32190ebf4e84168a7a227545
vklimjack@gmail.com <vklimjack@gmail.com>
2022-12-06 15:36:24 +0000

    day 6 part 1 solution

---
+++ AdventOfCodeDay6/Program.cs
@@ -0,0 +1,12 @@
+﻿var datastream = File.ReadAllText("C:\\Users\\vklim\\OneDrive\\Desktop\\Advent Of Code 2022\\aocday6.txt");
+
+for (int i = 0; i < datastream.Length - 3; i++)
+{
+    var sequence = datastream.Substring(i, 4);
+    if (sequence.Distinct().Count() == sequence.Length)
+    {
+        return i + 4;
+    }
+}
+
+return 0;
